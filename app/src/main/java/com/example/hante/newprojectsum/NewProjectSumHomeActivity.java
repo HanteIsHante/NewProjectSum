d0fe31b65f2e9d8edbe3d6878684d72559f3bd30
HanteIsHante <13051551994@163.com>
2016-08-25 23:12:48 +0800

    No.1

---
+++ app/src/main/java/com/example/hante/newprojectsum/NewProjectSumHomeActivity.java
@@ -0,0 +1,13 @@
+package com.example.hante.newprojectsum;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewProjectSumHomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_project_sum_home);
+    }
+}
