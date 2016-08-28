2024d2c8e734b4a572a811549344e9f232977e53
HanteIsHante <13051551994@163.com>
2016-08-28 19:53:12 +0800

    沉浸状态栏（Navigationview + toolbar）

---
+++ app/src/main/java/com/example/hante/newprojectsum/BaseActivity.java
@@ -0,0 +1,34 @@
+package com.example.hante.newprojectsum;
+
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.WindowManager;
+
+/**
+ * Created by handan on 2016/8/28.
+ */
+public class BaseActivity  extends AppCompatActivity {
+
+    @Override
+    protected void onCreate( Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        titleBarTop();
+    }
+    public void titleBarTop(){
+        // 状态栏设置透明
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {//5.0及以上
+            View decorView = getWindow().getDecorView();
+            int option = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+            decorView.setSystemUiVisibility(option);
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {//4.4到5.0
+            WindowManager.LayoutParams localLayoutParams = getWindow().getAttributes();
+            localLayoutParams.flags = (WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS | localLayoutParams.flags);
+        }
+    }
+}
--- app/src/main/java/com/example/hante/newprojectsum/NewProjectSumHomeActivity.java
+++ app/src/main/java/com/example/hante/newprojectsum/NewProjectSumHomeActivity.java
@@ -1,13 +1,85 @@
 package com.example.hante.newprojectsum;
 
-import android.support.v7.app.AppCompatActivity;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.internal.NavigationMenuView;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-public class NewProjectSumHomeActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class NewProjectSumHomeActivity extends BaseActivity {
+
+    @Bind(R.id.design_navigation_view)
+    NavigationView designNavigationView;
+    @Bind(R.id.drawerlayout)
+    DrawerLayout drawerlayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_project_sum_home);
+        ButterKnife.bind(this);
+        navViewtoTop();
+        initUI();
     }
+
+    private void navViewtoTop() {
+//        当系统版本小于5.0时，进行如下设置：
+        if(Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP){
+            drawerlayout.setFitsSystemWindows(true);
+            drawerlayout.setClipToPadding(false);
+        }
+    }
+
+    private void initUI() {
+        // 得到 顶部头像 Image
+        View headerView = designNavigationView.getHeaderView(0);
+        ImageView backgroundImg = (ImageView) headerView.findViewById(R.id.nav_head_background_img);
+        ImageView viewById = (ImageView) headerView.findViewById(R.id.user_photo);
+
+        designNavigationView.setItemIconTintList(null);// 图片本身颜色，不是统一颜色
+        MenuItem menuItem = designNavigationView.getMenu().findItem(R.id.moments);
+//        menuItem.setVisible(false);//true  显示 false 隐藏
+        removeNavigationViewScroller(designNavigationView);  //去掉侧边滚动条
+        clickOutNavigationViewClose();// 点击侧边框外部，关闭NavigationView
+
+
+    }
+
+    private void clickOutNavigationViewClose() {
+        designNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                switch (item.getItemId()){
+                    case R.id.qq:
+                        Toast.makeText(NewProjectSumHomeActivity.this, "QQ", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.qzone:
+                        Toast.makeText(NewProjectSumHomeActivity.this, "QZone", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+                item.setCheckable(false);
+                drawerlayout.closeDrawers();
+                return true;
+            }
+        });
+    }
+
+    private void removeNavigationViewScroller(NavigationView navigationView) {
+        if (navigationView != null) {
+            NavigationMenuView childAt = (NavigationMenuView) navigationView.getChildAt(0);
+            if (childAt != null) {
+                childAt.setVerticalScrollBarEnabled(false);
+            }
+        }
+    }
+
 }
