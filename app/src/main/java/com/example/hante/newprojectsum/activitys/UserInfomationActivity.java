a404089dc433eff61783a9225ce2eb0b4909638f
HanteIsHante <13051551994@163.com>
2016-09-02 19:54:00 +0800

    添加用户信息页面

---
+++ app/src/main/java/com/example/hante/newprojectsum/activitys/UserInfomationActivity.java
@@ -0,0 +1,15 @@
+package com.example.hante.newprojectsum.activitys;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.hante.newprojectsum.R;
+
+public class UserInfomationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_infomation);
+    }
+}
