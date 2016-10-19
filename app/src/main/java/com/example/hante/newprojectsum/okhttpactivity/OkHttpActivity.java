21cb3702cbbde3aec474a6ee708dba4f60e04d84
Hante <13051551994@163.com>
2016-10-19 20:42:34 +0800

    OkHttpManager 添加

---
+++ app/src/main/java/com/example/hante/newprojectsum/okhttpactivity/OkHttpActivity.java
@@ -0,0 +1,15 @@
+package com.example.hante.newprojectsum.okhttpactivity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.hante.newprojectsum.R;
+
+public class OkHttpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate (Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ok_http);
+    }
+}
