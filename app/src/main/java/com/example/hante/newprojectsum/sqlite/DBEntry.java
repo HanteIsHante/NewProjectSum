7f9f4af71f085e227053195d3e65d5b09fa1c0e0
Hante <13051551994@163.com>
2016-11-09 18:29:08 +0800

    添加数据库操作（增删改查）

---
+++ app/src/main/java/com/example/hante/newprojectsum/sqlite/DBEntry.java
@@ -0,0 +1,24 @@
+package com.example.hante.newprojectsum.sqlite;
+
+import android.provider.BaseColumns;
+
+/**
+ * 数据库表 字段
+ */
+
+public final class DBEntry {
+
+    public DBEntry () {
+    }
+
+    /**
+     * User 用户表
+     */
+    public static abstract class UserTable implements BaseColumns{
+            public static final String TABLE_NAME = "userInfo";
+            public static final String USER_NAME = "userName";
+            public static final String USER_PASSWORD ="userPassword";
+            public static final String USER_EMAIL = "userEmail";
+    }
+
+}
--- app/src/main/java/com/example/hante/newprojectsum/sqlite/DBHelper.java
+++ app/src/main/java/com/example/hante/newprojectsum/sqlite/DBHelper.java
@@ -18,13 +18,20 @@ public class DBHelper extends SQLiteOpenHelper{
     private static final String NUMBER_TYPE = " INTEGER";
     private static final String COMMA_SEP = ",";
 
+    private static final String USER_SQL =
+            "CREATE TABLE IF NOT EXISTS" + DBEntry.UserTable.TABLE_NAME +
+                    " (" + DBEntry.UserTable._ID + TEXT_TYPE + "PRIMARY KEY AUTOINCREMENT," +
+                    DBEntry.UserTable.USER_NAME + TEXT_TYPE + COMMA_SEP +
+                    DBEntry.UserTable.USER_PASSWORD + TEXT_TYPE + COMMA_SEP +
+                    DBEntry.UserTable.USER_EMAIL + TEXT_TYPE +
+                    " )";
     public DBHelper (Context context) {
         super(context, DATEBASE_NAME, null, DATEBASE_VERSION);
     }
 
     @Override
     public void onCreate (SQLiteDatabase sqLiteDatabase) {
-
+        sqLiteDatabase.execSQL(USER_SQL);
     }
 
     @Override
